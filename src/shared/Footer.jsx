478d087499f5218f146f4d36ed81bb3ee902d3b1
PayelOfficial <145429069+PayelOfficial@users.noreply.github.com>
2024-02-10 12:00:04 +0530

    Initial commit

---
+++ src/shared/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='bg-gray-200 py-4'>
+      <div className='container mx-auto flex justify-center items-center'>
+        <p className='text-gray-600'>Footer</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
